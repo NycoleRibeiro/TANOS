3c0e8ef1788b282db4d843351e11742d97d515e3
Nycole Ribeiro <rpnyck@gmail.com>
2023-08-16 16:30:46 -0300

    Create loaders

---
+++ src/components/loaders/blurLoader/index.tsx
@@ -0,0 +1,15 @@
+import './style.sass'
+
+export const BlurLoader = () => {
+  return (
+    <div className="loader">
+        <span>L</span>
+        <span>O</span>
+        <span>A</span>
+        <span>D</span>
+        <span>I</span>
+        <span>N</span>
+        <span>G</span>
+    </div>
+  )
+}
---
+++ src/components/loaders/typingLoader/index.tsx
@@ -0,0 +1,11 @@
+import './style.sass'
+
+export const TypingLoader = () => {
+  return (
+    <div className="loader">
+        <div className="scanner">
+            <span>Loading...</span>
+        </div>
+    </div>
+  )
+}
