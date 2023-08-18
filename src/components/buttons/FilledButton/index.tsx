ca8c4ec8b9f1f2c6e565e2d45028ccd64e157e2d
Nycole Ribeiro <rpnyck@gmail.com>
2023-08-18 17:43:10 -0300

    Update Filled button

--- src/components/buttons/FilledButton/index.tsx
+++ src/components/buttons/FilledButton/index.tsx
@@ -3,16 +3,18 @@ import './style.sass'
 
 interface ButtonProps {
   text: string;
+  icon?: string;
   onClick?: () => void;
 }
 
-export const FilledButton: React.FC<ButtonProps> = ({ text, onClick }) => {
+export const FilledButton: React.FC<ButtonProps> = ({ text, onClick, icon }) => {
   return (
     <button 
     className="filled-button"
     type={onClick ? 'button' : 'submit'}
     onClick={onClick}
     >
+      {icon && <img className="icon" src={icon} alt="icon" />}
       {text}
     </button>
   );
