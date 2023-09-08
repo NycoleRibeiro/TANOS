08d0184526826064d7ee8f06a1e86e2b3479edac
Nycole Ribeiro <rpnyck@gmail.com>
2023-09-08 19:03:27 -0300

    Create Input

---
+++ src/components/inputs/input/index.tsx
@@ -0,0 +1,26 @@
+import React, { ChangeEvent } from 'react'
+import './style.sass'
+
+interface InputProps {
+    label: string;
+    placeholder: string;
+    value: string;
+    name: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input: React.FC<InputProps> = ({ label, placeholder, value, name, onChange }) => {
+
+  return (
+    <div className="coolinput">
+        <label htmlFor="input" className="text">{label}</label>
+        <input 
+        type="text" 
+        placeholder={placeholder} 
+        name={name} 
+        value={value}
+        className="input"
+        onChange={onChange}/>
+    </div>
+  )
+}
