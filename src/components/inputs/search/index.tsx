addba0fba56ff0797d5e96fb99d712f3c0c8369e
Nycole Ribeiro <rpnyck@gmail.com>
2023-11-15 13:33:44 -0300

    Update search input

--- src/components/inputs/search/index.tsx
+++ src/components/inputs/search/index.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.sass'
 
 import SearchIcon from '../../../assets/images/search.svg'
 
 interface SearchInputProps {
-    placeholder: string;
-    onClick: () => void;
+  placeholder: string
+  onSearchChange: (searchTerm: string) => void
 }
 
-export const SearchInput: React.FC<SearchInputProps> = ({ placeholder, onClick }) => {
+export const SearchInput: React.FC<SearchInputProps> = ({
+  placeholder,
+  onSearchChange,
+}) => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
+    onSearchChange(e.target.value)
+  }
+
   return (
     <div className="searchBox">
-        <input className="searchInput" type="text" name="" placeholder={placeholder} />
-        <button className="searchButton" onClick={onClick}>
-            <img src={SearchIcon} alt="" />
-        </button>
+      <input
+        className="searchInput"
+        type="text"
+        placeholder={placeholder}
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button className="searchButton">
+        <img src={SearchIcon} alt="" />
+      </button>
     </div>
   )
 }
