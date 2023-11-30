8633c232af9ef2ad35bedba10e2cf4f43b1d748a
Nycole Ribeiro <rpnyck@gmail.com>
2023-11-30 19:50:55 -0300

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './style.sass'
 
+import { getProjects } from '../../database/Projects'
+import { Client, Project, Service } from '../../database/Types'
 import { getUserData } from '../../loggedUser'
 
+import serviceIcon from '../../assets/images/group.svg'
+import projectIcon from '../../assets/images/table-add.svg'
+import clientIcon from '../../assets/images/user-add-outlined.svg'
+import { Header } from '../../components/header'
 import { Sidebar } from '../../components/sidebar'
 
 export const Home = () => {
-  console.log(getUserData())
+  const navigate = useNavigate()
+
+  const user = getUserData()
+  const [naoIniciados, setNaoIniciados] = useState<Project[]>([])
+
+  useEffect(() => {
+    if (user) {
+      setNaoIniciados(getProjects(user.userId, 'Não iniciado'))
+    }
+  }, [user])
+
+  const renderProjectCard = (project: Project) => (
+    <div
+      className="card"
+      key={project.projectId}
+      onClick={() => {
+        navigate(`/projeto/${project.projectId}`)
+      }}
+    >
+      <div className="title">{project.titulo}</div>
+      <div className="price">
+        R${' '}
+        {project.gastos.reduce((acc, curr) => acc + curr.valor, 0).toFixed(2)}
+      </div>
+      <div className="info">
+        <span className="date">{project.dataEntrega}</span>
+        <span className="client">Cliente ID: {project.clienteId}</span>
+      </div>
+    </div>
+  )
 
   return (
     <div className="dashboard-container">
       <Sidebar activePage="Home" />
       <div className="content">
-        <h1>Dashboard</h1>
+        <Header path={[{ label: 'Início', path: '/home' }]} />
+        <div className="body">
+        <div className="projetosAndamento">
+            <div className="header">Projetos em andamento</div>
+            <div className="projetosCards">
+              {naoIniciados.map(renderProjectCard)}
+            </div>
+          </div>
+          <div className="atalhos">
+            <div className="atalho">
+              <div className="icon">
+                <img src={clientIcon} alt="" />
+              </div>
+              <div className="name">Cadastrar cliente</div>
+            </div>
+            <div className="atalho">
+              <div className="icon">
+                <img src={serviceIcon} alt="" />
+              </div>
+              <div className="name">Cadastrar serviço</div>
+            </div>
+            <div className="atalho">
+              <div className="icon">
+                <img src={projectIcon} alt="" />
+              </div>
+              <div className="name">Criar projeto</div>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   )
--- src/pages/Relatorios/index.tsx
+++ src/pages/Relatorios/index.tsx
@@ -1,5 +1,7 @@
 import './style.sass'
 
+import { Header } from '../../components/header'
+import { SingleSelect } from '../../components/inputs/SingleSelect'
 import { Sidebar } from '../../components/sidebar'
 
 import { getUserData } from '../../loggedUser'
@@ -9,7 +11,72 @@ export const Relatorios = () => {
     <div className="relatorios-container">
       <Sidebar activePage="Relatorios" />
       <div className="content">
-        <h1>Relatórios</h1>
+        <Header path={[{ label: 'Relatórios', path: '/home' }]} />
+        <div className="subheader">
+          <div className="title">Relatórios de Dezembro / 2023</div>
+          <div className="dataSelection">
+            <SingleSelect
+              onSelect={() => {}}
+              placeholder="Mês"
+              label="Mês"
+              options={[
+                'Janeiro',
+                'Fevereiro',
+                'Março',
+                'Abril',
+                'Maio',
+                'Junho',
+                'Julho',
+                'Agosto',
+                'Setembro',
+                'Outubro',
+                'Novembro',
+                'Dezembro',
+              ]}
+              initialValue={'Dezembro'}
+            />
+            <SingleSelect
+              onSelect={() => {}}
+              placeholder="Ano"
+              label="Ano"
+              options={['2023', '2024']}
+              initialValue={'2023'}
+            />
+          </div>
+        </div>
+        <div className="body">
+          <div className="valores">
+            <div className="card">
+              <div className="text">Lucro dos Projetos</div>
+              <div className="valor">R$5.214,00</div>
+            </div>
+            <div className="card">
+              <div className="text">Gastos do mês</div>
+              <div className="valor">R$245,91</div>
+            </div>
+            <div className="card">
+              <div className="text">Restante</div>
+              <div className="valor">R$2.878,09</div>
+            </div>
+          </div>
+          <div className="relatorioGeral">
+            <div className="header">Relatório Geral</div>
+            <div className="content">
+              <div className="dado">
+                <div className="number">3</div>
+                <div className="text">Novos Clientes Cadastrados</div>
+              </div>
+              <div className="dado">
+                <div className="number">5</div>
+                <div className="text">Novos Projetos Solicitados</div>
+              </div>
+              <div className="dado">
+                <div className="number">7</div>
+                <div className="text">Projetos Concluídos</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
